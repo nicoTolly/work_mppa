28ad2ac64de4e913e62c38ac9e1814af181f13da
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2016-11-14 17:42:59 +0100

    dcdv

--- loop_host.c
+++ loop_host.c
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <stdio.h>
+#include "ocl_utilities.h"
 #include <stdio.h>
 #include <stdlib.h>
 #define N (1 << 18)
@@ -9,10 +12,14 @@ int main()
   for(int i = 0; i < N ; i++)
     tab[i] = 1.2 + i * 3.5;
 
-#pragma scop
-  for(int i = 0; i < N ; i++)
-    dst[i] = tab[i] * tab[i] ;
-#pragma endscop
+  /* ppcg generated CPU code */
+  
+  {
+    int i;
+    for (i = 0; i < (1 << 18); i += 1) {
+      dst[i] = (tab[i] * tab[i]);
+    }
+  }
   sum = 0;
   for(int i = 0; i < N ; i++)
     sum += dst[i];
